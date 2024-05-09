78f34479bf71c5485929b7e63fae75e5147fb4db
Siddhesh Shende <138310226+siddheshshende@users.noreply.github.com>
2024-05-10 01:45:30 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Component/Home';
+import About from './Component/About';
+import News from './Component/News';
+import Contact from './Component/Contact';
+import Error from './Component/Error';
+import List from './Component/List';
+import Footer from './Component/Footer';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <List />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer/>
+    </>
   );
-}
+};
 
 export default App;
